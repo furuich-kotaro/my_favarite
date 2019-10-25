f7e7fb4d3797901e277b14a28d0be5738668ce22
Kotaro_Furuich <happy1998k@gmai.com>
2019-10-25 19:45:30 +0900

    userモデルの単体テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
           :recoverable, :rememberable, :validatable
 
   mount_uploader :image, ImageUploader
+  validates :name, presence: true, length: { maximum: 10 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "名前、メール、、イメージ、パスワードがあれば有効" do
+    user = create(:user)
+    expect(user).to be_valid
+  end
+
+  it "名前、メール、パスワードがあれば有効" do
+    user = create(:user, image: nil)
+    expect(user).to be_valid
+  end
+
+  it "名前は10文字だと有効" do
+    user = create(:user, name: "a"*10)
+    expect(user).to be_valid
+  end
+
+  it "名前は11文字だと無効" do
+    user = build(:user, name: "a"*11)
+    user.valid?
+    expect(user.errors[:name]).to include("は10文字以内で入力してください")
+  end
+
+  it "名前が空だと無効" do
+    user = build(:user, name: nil)
+    user.valid?
+    expect(user.errors[:name]).to include("を入力してください")
+  end
+
+  it "メールが空だと無効" do
+    user = build(:user, email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("を入力してください")
+  end
+
+  it "既存のメールアドレスは無効" do
+    user = create(:user, email: "example@test.com")
+    other_user = build(:user, email: "example@test.com")
+
+    other_user.valid?
+    expect(other_user.errors[:email]).to include("はすでに存在します")
+  end
 end
