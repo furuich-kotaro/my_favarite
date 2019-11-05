46cf4a9e645beaac839f4c375b216a2ca7b2099c
Kotaro_Furuich <happy1998k@gmai.com>
2019-11-05 16:24:37 +0900

    ページネーションを追加

--- Gemfile
+++ Gemfile
@@ -46,6 +46,7 @@ gem 'dotenv-rails'
 gem 'pry-rails'
 gem "gmaps4rails"
 gem "geocoder"
+gem 'kaminari'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :destroy]
 
   def index
-    @posts = Post.all
+    @posts = Post.all.page(params[:page]).per(3)
   end
 
   def new
