ed30e8fe42f3c27198bd0416d38d4dc4ccf8d5a5
Kotaro_Furuich <happy1998k@gmai.com>
2019-11-14 01:59:36 +0900

    簡単ログインでログインしてもユーザ情報を変更できるように修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,9 +5,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+    super
+    @user = User.first
+  end
 
   # POST /resource
   # def create
