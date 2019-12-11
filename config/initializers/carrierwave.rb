96df30d875fd835bb5795eb7e9cf396fa8cd5406
furuichi <happy1998k@gmai.com>
2019-12-11 15:27:17 +0900

    本番環境でのcarrierwaveがS３を使用するように変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,17 +1,22 @@
-# require 'carrierwave/storage/abstract'
-# require 'carrierwave/storage/file'
-# require 'carrierwave/storage/fog'
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
 
-# CarrierWave.configure do |config|
-#   config.storage = :fog
-#   config.fog_provider = 'fog/aws'
-#   config.fog_credentials = {
-#     provider: 'AWS',
-#     aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
-#     aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
-#     region: 'ap-northeast-1'
-#   }
+CarrierWave.configure do |config|
+  if Rails.env.production? # 本番環境:AWS使用
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+      region: 'ap-northeast-1'
+    }
 
-#   config.fog_directory = 'myfavarite-s3'
-#   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/myfavarite-s3'
-# end
+    config.fog_directory = 'myfavarite-s3'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/myfavarite-s3'
+  else
+    config.storage :file # 開発環境:public/uploades下に保存
+    config.enable_processing = false if Rails.env.test? # test:処理をスキップ
+  end
+end
