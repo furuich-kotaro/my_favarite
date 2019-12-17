ce88379d5c83d5491bc60888377dcaaf6b715ab9
furuichi <happy1998k@gmai.com>
2019-12-17 17:13:21 +0900

    #indexと#searchのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,13 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :users, only: [:show] do
     member do
-      get 'search',        to: 'users#search'
-      put 'follow',        to: 'users#follow'
-      put 'unfollow',      to: 'users#unfollow'
-      get 'follow_list',   to: 'users#follow_list'
-      get 'follower_list', to: 'users#follower_list'
+      get 'search',            to: 'users#search'
+      put 'follow',            to: 'users#follow'
+      put 'unfollow',          to: 'users#unfollow'
+      get 'follow_list',       to: 'users#follow_list'
+      get 'follower_list',     to: 'users#follower_list'
+      get 'likes/list',        to: 'likes#index'
+      get 'likes/list/search', to: 'likes#search'
     end
   end
   resources :posts, except: [:show, :edit, :update] do
