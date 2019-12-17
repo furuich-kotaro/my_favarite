f69601605885e7b633591b32d14f40f70b3501b1
furuichi <happy1998k@gmai.com>
2019-12-17 17:13:54 +0900

    #indexと#searchをコントローラーに設定

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,6 +1,12 @@
 class LikesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_post
+  before_action :set_post, only: [:create, :destroy]
+
+  def index
+    likes = Like.where(user_id: params[:id]).pluck(:post_id)
+    @posts = Post.where(id: likes).includes(:taggings, :user).page(params[:page]).per(9).order('created_at DESC')
+    @like = Like.new
+  end
 
   def create
     @like = current_user.likes.create(post_id: params[:post_id])
@@ -13,9 +19,20 @@ class LikesController < ApplicationController
     @post.reload
   end
 
+  def search
+    likes = Like.where(user_id: params[:id]).pluck(:post_id)
+    @q = Post.search(search_params)
+    @posts = @q.result(distinct: true).where(id: likes).includes(:taggings, :user).page(params[:page]).per(9).order('created_at DESC')
+    @like = Like.new
+  end
+
   private
 
   def set_post
     @post = Post.find(params[:post_id])
+  end
+
+  def search_params
+    params.require(:q).permit(:address_cont)
   end
 end
