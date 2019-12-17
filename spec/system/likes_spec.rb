52e5fbd9103db1888a89a7c321f1af5bccb883e8
furuichi <happy1998k@gmai.com>
2019-12-17 18:30:14 +0900

    いいねした投稿一覧のsystemテストを追加

--- spec/system/likes_spec.rb
+++ spec/system/likes_spec.rb
@@ -1,11 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe "Likes", type: :system, js: true, retry: 5  do
+RSpec.describe "Likes", type: :system  do
   let(:user) { create(:user)}
   let(:other_user) { create(:user)}
 
+  describe 'index' do
+    scenario 'いいねした投稿一覧を表示できる' do
+      like_post = create(:post, user: other_user)
+      post = create(:post_kawaguchiko, user: user)
+      create(:like, post: like_post, user: user)
+      sign_in user
+      visit root_path
+      expect(page).to have_content '東京'
+      expect(page).to have_content '河口湖'
+      visit likes_list_user_path(user)
+      expect(page).to have_content '東京'
+      expect(page).not_to have_content '河口湖'
+    end
+  end
+
   describe 'create' do
-    scenario 'ログイン済みユーザ/他人の投稿にはいいねできる', js: true  do
+    scenario 'ログイン済みユーザ/他人の投稿にはいいねできる', js: true, retry: 5  do
       post = create(:post, user: user)
       sign_in other_user
       visit root_path
@@ -34,7 +49,7 @@ RSpec.describe "Likes", type: :system, js: true, retry: 5  do
   end
 
   describe 'destroy' do
-    scenario 'ログイン済みユーザはいいねを削除できる', js: true  do
+    scenario 'ログイン済みユーザはいいねを削除できる', js: true, retry: 5 do
       post = create(:post, user: user)
       create(:like, post: post, user: other_user)
       sign_in other_user
@@ -53,6 +68,26 @@ RSpec.describe "Likes", type: :system, js: true, retry: 5  do
       visit root_path
       expect(page).not_to have_css('.likes-link-delete')
       expect(page).to have_css('.gray_heart')
+    end
+  end
+
+  describe 'search' do
+    scenario 'いいねした投稿一覧を表示できる' do
+      like_post_tokyo = create(:post, user: other_user)
+      like_post_kawaguchi = create(:post_kawaguchiko, user: other_user)
+      create(:like, post: like_post_tokyo, user: user)
+      create(:like, post: like_post_kawaguchi, user: user)
+
+      sign_in user
+      visit likes_list_user_path(user)
+      expect(page).to have_content '東京'
+      expect(page).to have_content '河口湖'
+
+      fill_in 'userSearchFiled', with: '東京'
+      find(".userSearchSubmit").click
+
+      expect(page).to have_content '東京'
+      expect(page).not_to have_content '河口湖'
     end
   end
 end
