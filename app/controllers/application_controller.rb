3183dc5516b2aee0e97408f03b2b2fa581a86292
furuichi <happy1998k@gmai.com>
2019-12-20 22:33:23 +0900

    未確認通知を取得するメソッドを変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_racsack
+  before_action :set_notifications
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
@@ -19,5 +20,9 @@ class ApplicationController < ActionController::Base
       marker.infowindow post.address
       marker.infowindow render_to_string(partial: 'shard/infowindow', locals: { post: post })
     end
+  end
+
+  def set_notifications
+    @notifications = current_user.passive_notifications.where(checked: false).includes(:visitor, :post) if signed_in?
   end
 end
--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,7 +1,12 @@
 class NotificationsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @notifications_list = current_user.passive_notifications.includes(:visitor, :post).page(params[:page]).per(20)
+  end
+
   def update
-    @notifications = current_user.passive_notifications
-    @notifications.where(checked: false).each do |notification|
+    current_user.passive_notifications.where(checked: false).each do |notification|
       notification.update(checked: true)
     end
   end
