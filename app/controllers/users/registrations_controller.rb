8ce616be7875e45c794b3a6ae975e53b59ab50dd
furuichi <happy1998k@gmai.com>
2019-12-23 15:49:03 +0900

    新規登録ページの簡単ログインを修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,10 +5,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  def new
-    super
-    @user = User.first
-  end
+  # def new
+  #   super
+  # end
 
   # POST /resource
   # def create
